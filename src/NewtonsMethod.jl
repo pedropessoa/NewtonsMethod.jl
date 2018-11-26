7e38028215b325a865ec2ec648a0442f3ddd9e76
Pedro Pessoa <pedromp17@gmail.com>
2018-11-25 23:29:38 -0800

    Update NewtonsMethod.jl

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,33 @@
 module NewtonsMethod
 
-greet() = print("Hello World!")
+using LinearAlgebra, Statistics, Compat, ForwardDiff
 
-end # module
+function newtonroot(f, f_prime; x_0, tolerance = 1E-7, maxiter = 1000)
+    x_old = x_0
+    error = Inf
+    iter = 1
+    while error > tolerance && iter <= maxiter
+        if f_prime(x_old) ≈ 0.0
+            return (root = nothing, error = nothing, iter = nothing)
+        end
+        x_new = x_old - f(x_old)/f_prime(x_old)
+        error = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    if iter == maxiter+1
+        return (root = nothing, error = nothing, iter = nothing)
+    else
+        return (root = x_old, error = error, iter = iter)
+    end
+end
+
+# Applying auto-differentiation
+D(f) = x -> ForwardDiff.derivative(f, x)
+
+newtonroot(f; x_0, tolerance = 1E-7, maxiter = 1000) = newtonroot(f, D(f), x_0 = x_0, tolerance = tolerance, maxiter = maxiter)
+
+# Export function
+export newtonroot
+
+end
